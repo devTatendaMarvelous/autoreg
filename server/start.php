206bc405614a87f392189d30509542df27ed1635
devTatendaMarvelous <marveloustchimusoro@gmail.com>
2026-03-30 16:03:13 +0200

    Server scipt

--- server/start.php
+++ server/start.php
@@ -4,7 +4,7 @@ require 'HttpServer.php';
 $ip = '0.0.0.0';
 $port = 8004;
 $username = 'admin';
-$password = 'Corepay@1';
+$password = 'unimills2026';
 
 $server = new HttpServer($ip, $port, $username, $password);
 $server->start();
